d2ed51b6034ed53b9f4e800646e1a2dfb193803b
981778103@qq.com <ajie>
2021-01-15 21:53:29 +0800

    源码更新

--- 03-js/案例/图片库/js/main.js
+++ 03-js/案例/图片库/js/main.js
@@ -1,9 +1,54 @@
+window.onload = addLoadEvent;
+
+addLoadEvent(prepareGallery);
+
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof oldonload != "function"){
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function prepareGallery(){
+    //检查点
+    if(!document.getElementsByTagName){
+        return false;
+    }
+    if(!document.getElementById){
+        return false;
+    }
+    if(!document.getElementById("imagegallery")){
+        return false;
+    }
+    var imagegallery = document.getElementById("imagegallery");
+    var links = imagegallery.getElementsByTagName("a");
+
+    for(var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return !showPic(this);
+        }
+    }
+}
+
 function showPic(whichpic) {
-    var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
-    var text = whichpic.getAttribute("title");
     var description = document.getElementById("description");
 
+    if(!placeholder) {
+        return false;
+    }
+
+    var source = whichpic.getAttribute("href");
     placeholder.setAttribute("src", source);
-    description.firstChild.nodeValue = text;
+    
+    if(description) {
+        var text = whichpic.getAttribute("title");
+        description.firstChild.nodeValue = text;
+    }
+    return true;
 }
